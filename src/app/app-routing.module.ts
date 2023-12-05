852933c921a075adc4e9d8e8712ac4b6e16fdc61
Scaffolder <scaffolder@backstage.io>
2023-12-05 10:21:36 +0000

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateCustomerProfileComponent } from "./customer-profile/create-customer-profile.component";
+import { ListCustomerProfilesComponent } from "./customer-profile/list-customer-profiles.component";
+
+
+const routes: Routes = [
+  { path: 'customer-profiles/list', component: ListCustomerProfilesComponent },
+  { path: 'customer-profiles/create', component: CreateCustomerProfileComponent },
+  
+  { path: '', redirectTo: '/customer-profiles/list', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
